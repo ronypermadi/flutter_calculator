866ae7d8a62f4f3653eb3ada538d311f27846f7a
ronypermadi <ronnypermadi1@gmail.com>
2020-11-02 16:20:23 +0700

    basic layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -10,9 +11,14 @@ class MainApp extends StatefulWidget {
 
 class CalculatorAppState extends State<MainApp> {
   final double _padding = 16.0;
+  final double _buttonFontSize = 24.0;
 
   final Color _primarySwatchColor = Colors.orange;
   final Color _titleAppBarColor = Colors.white;
+  final Color _buttonColorWhite = Colors.white;
+  final Color _buttonHighlightColor = Colors.grey[800];
+  final Color _buttonColorGrey = Colors.grey[500];
+  final Color _textColorWhite = Colors.white;
 
   @override
   Widget build(BuildContext context) {
@@ -34,15 +40,351 @@ class CalculatorAppState extends State<MainApp> {
                 key: Key("expanded_container_bagian_atas"),
                 width: double.infinity,
                 height: double.infinity,
+                padding: EdgeInsets.all(_padding),
+                child: Stack(
+                  alignment: Alignment.bottomRight,
+                  children: <Widget>[
+                    AutoSizeText(
+                      "0",
+                      style: Theme.of(context).textTheme.display2,
+                      maxLines: 1,
+                    ),
+                  ],
+                ),
               ),
             ),
             Expanded(
               key: Key("expanded_bagian_bawah"),
               flex: 1,
-              child: Container(
-                key: Key("expanded_container_bagian_bawah"),
-                width: double.infinity,
-                height: double.infinity,
+              child: Column(
+                key: Key("expanded_column_bagian_bawah"),
+                children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Expanded(
+                          flex: 2,
+                          child: RaisedButton(
+                            color: _buttonColorWhite,
+                            highlightColor: _buttonHighlightColor,
+                            child: Text(
+                              "C",
+                              style: TextStyle(
+                                  color: _primarySwatchColor,
+                                  fontSize: _buttonFontSize),
+                            ),
+                            onPressed: () {
+                              // TODO: do something in here
+                            },
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: RaisedButton(
+                            color: _buttonColorWhite,
+                            highlightColor: _buttonHighlightColor,
+                            child: Icon(
+                              Icons.backspace,
+                              color: _buttonColorGrey,
+                            ),
+                            onPressed: () {
+                              // TODO: do something in here
+                            },
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: RaisedButton(
+                            color: _buttonColorWhite,
+                            highlightColor: _buttonHighlightColor,
+                            child: Text(
+                              "/",
+                              style: TextStyle(
+                                color: _buttonColorGrey,
+                                fontSize: _buttonFontSize,
+                              ),
+                            ),
+                            onPressed: () {
+                              // TODO: do something in here
+                            },
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Expanded(
+                          flex: 1,
+                          child: RaisedButton(
+                            color: _buttonColorWhite,
+                            highlightColor: _buttonHighlightColor,
+                            child: Text(
+                              "7",
+                              style: TextStyle(
+                                color: _buttonColorGrey,
+                                fontSize: _buttonFontSize,
+                              ),
+                            ),
+                            onPressed: () {
+                              // TODO: do something in here
+                            },
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: RaisedButton(
+                            color: _buttonColorWhite,
+                            highlightColor: _buttonHighlightColor,
+                            child: Text(
+                              "8",
+                              style: TextStyle(
+                                color: _buttonColorGrey,
+                                fontSize: _buttonFontSize,
+                              ),
+                            ),
+                            onPressed: () {
+                              // TODO: do something in here
+                            },
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: RaisedButton(
+                            color: _buttonColorWhite,
+                            highlightColor: _buttonHighlightColor,
+                            child: Text(
+                              "9",
+                              style: TextStyle(
+                                color: _buttonColorGrey,
+                                fontSize: _buttonFontSize,
+                              ),
+                            ),
+                            onPressed: () {
+                              // TODO: do something in here
+                            },
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: RaisedButton(
+                            color: _buttonColorWhite,
+                            highlightColor: _buttonHighlightColor,
+                            child: Text(
+                              "x",
+                              style: TextStyle(
+                                color: _buttonColorGrey,
+                                fontSize: _buttonFontSize,
+                              ),
+                            ),
+                            onPressed: () {
+                              // TODO: do something in here
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Expanded(
+                          flex: 1,
+                          child: RaisedButton(
+                            color: _buttonColorWhite,
+                            highlightColor: _buttonHighlightColor,
+                            child: Text(
+                              "4",
+                              style: TextStyle(
+                                color: _buttonColorGrey,
+                                fontSize: _buttonFontSize,
+                              ),
+                            ),
+                            onPressed: () {
+                              // TODO: do something in here
+                            },
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: RaisedButton(
+                            color: _buttonColorWhite,
+                            highlightColor: _buttonHighlightColor,
+                            child: Text(
+                              "5",
+                              style: TextStyle(
+                                color: _buttonColorGrey,
+                                fontSize: _buttonFontSize,
+                              ),
+                            ),
+                            onPressed: () {
+                              // TODO: do something in here
+                            },
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: RaisedButton(
+                            color: _buttonColorWhite,
+                            highlightColor: _buttonHighlightColor,
+                            child: Text(
+                              "6",
+                              style: TextStyle(
+                                color: _buttonColorGrey,
+                                fontSize: _buttonFontSize,
+                              ),
+                            ),
+                            onPressed: () {
+                              // TODO: do something in here
+                            },
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: RaisedButton(
+                            color: _buttonColorWhite,
+                            highlightColor: _buttonHighlightColor,
+                            child: Text(
+                              "-",
+                              style: TextStyle(
+                                color: _buttonColorGrey,
+                                fontSize: _buttonFontSize,
+                              ),
+                            ),
+                            onPressed: () {
+                              // TODO: do something in here
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Expanded(
+                          flex: 1,
+                          child: RaisedButton(
+                            color: _buttonColorWhite,
+                            highlightColor: _buttonHighlightColor,
+                            child: Text(
+                              "1",
+                              style: TextStyle(
+                                color: _buttonColorGrey,
+                                fontSize: _buttonFontSize,
+                              ),
+                            ),
+                            onPressed: () {
+                              // TODO: do something in here
+                            },
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: RaisedButton(
+                            color: _buttonColorWhite,
+                            highlightColor: _buttonHighlightColor,
+                            child: Text(
+                              "2",
+                              style: TextStyle(
+                                color: _buttonColorGrey,
+                                fontSize: _buttonFontSize,
+                              ),
+                            ),
+                            onPressed: () {
+                              // TODO: do something in here
+                            },
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: RaisedButton(
+                            color: _buttonColorWhite,
+                            highlightColor: _buttonHighlightColor,
+                            child: Text(
+                              "1",
+                              style: TextStyle(
+                                color: _buttonColorGrey,
+                                fontSize: _buttonFontSize,
+                              ),
+                            ),
+                            onPressed: () {
+                              // TODO: do something in here
+                            },
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: RaisedButton(
+                            color: _buttonColorWhite,
+                            highlightColor: _buttonHighlightColor,
+                            child: Text(
+                              "+",
+                              style: TextStyle(
+                                color: _buttonColorGrey,
+                                fontSize: _buttonFontSize,
+                              ),
+                            ),
+                            onPressed: () {
+                              // TODO: do something in here
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Expanded(
+                          flex: 3,
+                          child: RaisedButton(
+                            color: _buttonColorWhite,
+                            highlightColor: _buttonHighlightColor,
+                            child: Text(
+                              "0",
+                              style: TextStyle(
+                                color: _buttonColorGrey,
+                                fontSize: _buttonFontSize,
+                              ),
+                            ),
+                            onPressed: () {
+                              // TODO: do something in here
+                            },
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: RaisedButton(
+                            color: _primarySwatchColor,
+                            highlightColor: _buttonHighlightColor,
+                            child: Text(
+                              "=",
+                              style: TextStyle(
+                                color: _textColorWhite,
+                                fontSize: _buttonFontSize,
+                              ),
+                            ),
+                            onPressed: () {
+                              // TODO: do something in here
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             )
           ],
